353f0e5bd91a7559432af1397ee17c111ca77a8b
Anthony Her <antpher@gmail.com>
2021-12-12 16:55:03 -0500

    fixed some file paths

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,9 +1,9 @@
 const FILES_TO_CACHE = [
     "/index.html",
-    "/styles.css",
-    "/index.js",
+    "/css/styles.css",
+    "/js/index.js",
     "/manifest.webmanifest",
-    "/db.js"
+    "/js/idb.js"
 ];
 
 const APP_PREFIX = 'BudgetTracker-';
@@ -43,10 +43,10 @@ self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
         caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
+            if (request) {
                 console.log('responding with cache : ' + e.request.url)
                 return request
-            } else {       // if there are no cache, try fetching request
+            } else {
                 console.log('file is not cached, fetching : ' + e.request.url)
                 return fetch(e.request)
             }
